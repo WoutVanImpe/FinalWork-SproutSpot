1c496a12a5348423578fe67abe5e0b88b044133a
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2026-05-11 12:49:11 +0200

    feat(frontend-styling): added styling constants

---
+++ frontend/app/_layout.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+
+const Footer = () => {
+	return (
+		<View style={{ flex: 1 }}>
+			<Stack />
+			<Text>Footer</Text>
+		</View>
+	);
+};
+
+export default Footer;
+
+const styles = StyleSheet.create({});
---
+++ frontend/constants/Styling.ts
@@ -0,0 +1,53 @@
+export const Styling = {
+	Colors: {
+		green: "#00CA68",
+		red: "#C44028",
+		white: "#fff",
+		lightGrey: "#a1a1a1",
+		gradGrey: "linear-gradient(25deg,rgba(109, 118, 126, 1) 0%, rgba(69, 78, 86, 1) 100%)",
+	},
+
+	Fonts: {
+		Size: {
+			sml: 10,
+			reg: 14,
+			med: 18,
+			lrg: 24,
+			xlg: 32,
+		},
+
+		Weight: {
+			reg: 500,
+			bold: 700,
+		},
+	},
+
+	Spacing: {
+		xsm: 4,
+		sml: 8,
+		reg: 12,
+		med: 16,
+		lrg: 24,
+		xlg: 32,
+		xxl: 40,
+	},
+
+	Padding: {
+		xsm: 4,
+		sml: 8,
+		reg: 12,
+		med: 16,
+		lrg: 24,
+		xlg: 32,
+		xxl: 40,
+	},
+
+	BorderRadius: {
+		sml: 4,
+		reg: 8,
+		med: 32,
+		lrg: "50%",
+	},
+
+	Shadow: "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+};
