4040cfdf6aee53824e3a807a1c4e4c682c2659d2
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2026-05-13 12:09:42 +0200

    feat(explorepage): added auto scroll into view for the searchbar

--- frontend/components/pages/explore/vegetableList/VegetableList.tsx
+++ frontend/components/pages/explore/vegetableList/VegetableList.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, TextInput, View } from "react-native";
-import React from "react";
+import { Keyboard, LayoutChangeEvent, StyleSheet, TextInput, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
 import StyledText from "../../../style/StyledText";
 import StyledIcon from "../../../style/StyledIcon";
 import { VegetableCardProps } from "../../../shared/vegetableCard/VegetableCard";
@@ -7,22 +7,49 @@ import CardContainer from "../../../shared/vegetableCard/CardContainer";
 import SearchIcon from "../../../../assets/icons/search.svg";
 import { Styling } from "../../../../constants/Styling";
 import Spacer from "../../../style/Spacer";
-import { BAR_MARGIN } from "../../../../constants/tabConfig";
+import { useScroll } from "../../../../context/ScrollContext";
 
 interface VegetableListProps {
 	data: VegetableCardProps[];
 }
 
 const VegetableList = ({ data }: VegetableListProps) => {
+	const { scrollTo } = useScroll();
+	const inputRef = useRef<TextInput>(null);
+	const [containerY, setContainerY] = useState(0);
+	const [searchBarY, setSearchBarY] = useState(0);
+
+	useEffect(() => {
+		const hide = Keyboard.addListener("keyboardDidHide", () => {
+			inputRef.current?.blur();
+		});
+		return () => hide.remove();
+	}, []);
+
+	const handleContainerLayout = (e: LayoutChangeEvent) => {
+		setContainerY(e.nativeEvent.layout.y);
+	};
+
+	const handleSearchBarLayout = (e: LayoutChangeEvent) => {
+		setSearchBarY(e.nativeEvent.layout.y);
+	};
+
+	const handleFocus = () => {
+		const targetY = containerY + searchBarY - 50;
+		if (targetY > 0) {
+			scrollTo(targetY, true);
+		}
+	};
+
 	return (
-		<View style={styles.container}>
+		<View style={styles.container} onLayout={handleContainerLayout}>
 			<StyledText type="head2" style={styles.title}>
 				Alle groenten
 			</StyledText>
 			<Spacer space={Styling.Spacing.med} />
-			<View style={styles.searchBar}>
+			<View style={styles.searchBar} onLayout={handleSearchBarLayout}>
 				<StyledIcon Icon={SearchIcon} size="reg" fill={Styling.Colors.white} />
-				<TextInput style={styles.input} placeholder="Zoeken..." placeholderTextColor={Styling.Colors.white} />
+				<TextInput ref={inputRef} style={styles.input} placeholder="Zoeken..." placeholderTextColor={Styling.Colors.white} onFocus={handleFocus} />
 			</View>
 			<Spacer space={Styling.Spacing.reg} />
 			<CardContainer data={data} style={{ marginBottom: 175 }} />
