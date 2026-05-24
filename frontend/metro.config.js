ae9c5c4827b13c485a895876e6fee88b889d74fd
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2026-05-24 14:14:18 +0200

    fix(frontend): add ts/tsx to metro sourceExts for metro-runtime package

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -11,7 +11,7 @@ config.transformer = {
 config.resolver = {
   ...resolver,
   assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-  sourceExts: [...resolver.sourceExts, "svg"],
+  sourceExts: [...resolver.sourceExts, "svg", "ts", "tsx"],
 };
 
 module.exports = config;
